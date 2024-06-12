51adeaea2d002c8382dfd5ce4c53a6ac0652590a
ghoniem123 <mohamedaymanqwerty@gmail.com>
2024-06-12 16:01:10 +0300

    user verification by admin done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AddExam from "./pages/addExam";
 import HomePage from "./pages/homepage";
+import AdminVerification from "./pages/adminVerification";
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/addExam" element={<AddExam />} />
+        <Route path="/adminVerification" element={<AdminVerification />} />
       </Routes>
     </>
   );
---
+++ frontend/src/pages/adminVerification.jsx
@@ -0,0 +1,100 @@
+import Navbar from "../components/adminNavbar";
+import { useState } from "react";
+import axios from "axios";
+import { Button } from 'react-bootstrap';
+import '../styles/adminVerification.css';
+
+
+export default function AdminVerification(){
+    const [users, setUsers] = useState([
+        {
+            name: "Mohamed",
+            ID:"30305018025",
+            email:"mohaemd00@gmail.com",
+            verified:''
+        },
+        {
+            name: "Omar",
+            ID:"2020585282",
+            email:"omar123@gmail.com",
+            verified:''
+        },
+        {
+            name: "Ahmed",
+            ID:"3030501825",
+            email:"ahmed00@gmail.com",
+            verified:''
+        },
+        {
+            name: "Yehia",
+            ID:"530305501825",
+            email:"yehia00@gmail.com",
+            verified:''
+        },
+        {
+            name: "Mazen",
+            ID:"13030501825",
+            email:"mazen530@gmail.com",
+            verified:''
+        },
+        {
+            name: "Ali",
+            ID:"23030501825",
+            email:"ali00@gmail.com",
+            verified:''
+        }
+    ])
+
+    return(
+        <>
+        <Navbar/>
+        <div className='user--verification--screen--div'>
+        {users.map((user) => {
+            return(
+
+            user.verified === ''?
+            <div className='user--verification--div'>    
+
+                
+                    <h1>Name : <span style={{color:'#262626'}}>{user.name}</span></h1>
+                    <h2>National ID: <span style={{color:'#262626'}}>{user.ID}</span></h2>
+                    <h2>Email : <span style={{color:'#262626'}}>{user.email}</span></h2>
+                
+                <div className='button--div'>
+                <Button className ='approve--button' variant="success" onClick={()=>{
+                    setUsers(users.map((User) => {
+                        if(user.ID === User.ID){
+                            User.verified = 'Approved'
+                        }
+                        return User
+                    }))
+                }} >Verify</Button>
+                <Button className ='reject--button' variant="danger" onClick={()=>{
+                    setUsers(users.map((User) => {
+                        if(user.ID === User.ID){
+                            User.verified = 'Rejected'
+                        }
+                        return User
+                    }))
+                }} >Reject</Button>
+                </div>
+                </div>
+            :
+            <div className='user--verification--div'>    
+
+            
+                <h1>Name : <span style={{color:'#262626'}}>{user.name}</span></h1>
+                <h2>National ID: <span style={{color:'#262626'}}>{user.ID}</span></h2>
+                <h2>Email : <span style={{color:'#262626'}}>{user.email}</span></h2>
+                <h2>Verification Status : <span style={{ color: user.verified === 'Approved' ? '#43ab32' : '#BC54CD' }}>{user.verified}</span></h2>
+
+
+            </div> 
+            
+            )
+        })}
+        </div>
+        </>
+    )
+
+}
