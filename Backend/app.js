db35d6e59769669c5f08200f1389473ff9a9a47c
ghoniem123 <mohamedaymanqwerty@gmail.com>
2024-06-25 22:28:35 +0300

    backend

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ const http = require('http');
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io = socketIO(server, {
-  cors:{
+  cors:{ 
     origin:'*'
   }
 }).listen(4000);
